3ae0e7dad923eee0050f42a10cc82aaec0e20439
Alberto Lacerda <albertotads@outlook.com>
2026-03-18 19:48:02 -0300

    Criado arquivos kotlin

--- TiposVariaveis/Variaveis.kt
+++ TiposVariaveis/Variaveis.kt
@@ -5,5 +5,36 @@
     i = 123456789
     System.out.println("Tipo inteiro : $i")
 
+    //Tipo Char
+    val c: Char
+    c= '#'
+    System.out.println("Tipo char: $c")
+     // Tipo Float
+    val f: Float 
+    f = 145f
+    System.out.println("Tipo Float: $f") 
+
+    //tipo Double 
+    val d: Double
+    d = 650.50
+    System.out.println("Tipo Double : $d")
+
+    //Tipo Texto
+    val t: String
+    t = "Mensagem de Texto"
+    System.out.println(t)
+
+    //Tipo Long Numeros muito grandes 
+    val l: Long
+    l = 127_640_760_670_770_000L
+    System.out.println("Tipo Long: $l")
+
+    //Tipo Boolean 
+    val conf: Boolean
+    conf = true
+    System.out.println("Tipo Booleano: $conf" )
+
+    //Tipo Data ?
+   
 
 }
