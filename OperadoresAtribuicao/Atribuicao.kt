15e70be7f781c788cee492985352190d5ed22973
Alberto Lacerda <albertotads@outlook.com>
2026-03-21 15:51:49 -0300

    Adiocionado Sobre Operadores

---
+++ OperadoresAtribuicao/Atribuicao.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    //Atribuicao 
+    // +=
+    // var pode ser Alterada 
+    // val não se altera durante o codigo 
+    var n = 4
+    n +=n
+    System.out.println(n);
+    // -=
+    // ++
+    // --
+
+}
