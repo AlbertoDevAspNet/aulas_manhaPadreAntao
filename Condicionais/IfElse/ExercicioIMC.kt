ab5b5b12b82612b73ea2694cf111b195ef619c1f
Alberto Lacerda <albertotads@outlook.com>
2026-04-01 19:17:06 -0300

    Adicionado mais Arquivos

---
+++ Condicionais/IfElse/ExercicioIMC.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    /*
+    Criar um programa para calcular o IMC
+    Informar as seguintes Situações 
+    Menor que 18.5
+    18.5 a 24.9 Normal
+    25 a 29.9 Sobrepeso
+    30 a 34.9 Obesidade Grau 1
+    35 a 39.9 Obesidade Grau 2
+    Maior que 40 Obesidade Grau 3 
+     */
+}
